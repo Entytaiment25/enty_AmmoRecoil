fb618f4e492418333ebe57f9a290e19ded34985d
enty <64799287+Entytaiment25@users.noreply.github.com>
2024-11-19 19:59:15 +0100

    Added NoSpread, Added OnlyAllowedWeapons Option, code refactor

--- config.lua
+++ config.lua
@@ -1,4 +1,10 @@
 Config = {}
 
-Config.UnlimitedAmmo = true
+Config.AllowedWeapons = {
+    `WEAPON_PISTOL`,
+    -- Add more weapon hashes here
+}
 Config.NoRecoil = true
+Config.NoSpread = true
+Config.OnlyAllowedWeapons = true
+Config.UnlimitedAmmo = true
